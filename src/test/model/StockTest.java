23b3f966dd3e17fa53e9990d189d3d1e24314c10
The Nam Anh Mai <tnmai@students.cs.ubc.ca>
2024-10-24 02:07:39 -0700

    Added test for toJson in StockTest

--- src/test/model/StockTest.java
+++ src/test/model/StockTest.java
@@ -1,9 +1,11 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +21,12 @@ public class StockTest {
     void testConstructor() {
         assertEquals("AAPL", stock.getSymbol());
         assertEquals(new BigDecimal("226.80"), stock.getPrice());
+    }
+
+    @Test
+    void testToJson() {
+        String expectedJson = "{\"symbol\":\"AAPL\",\"price\":\"226.80\"}";
+        JSONObject jsonObject = stock.toJson();
+        assertTrue(jsonObject.similar(expectedJson));
     }
 }
