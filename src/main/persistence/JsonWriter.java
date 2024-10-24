3059dbe68360893587facac00cb6acd8ea4c3764
The Nam Anh Mai <tnmai@students.cs.ubc.ca>
2024-10-24 01:56:55 -0700

    Implemented Writable and JsonWriter classes

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -13,26 +13,32 @@ public class JsonWriter {
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of account to file
     public void write(Account account) {
+        JSONObject jsonObject = account.toJson();
+        saveToFile(jsonObject.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
+        writer.print(json);
     }
 }
