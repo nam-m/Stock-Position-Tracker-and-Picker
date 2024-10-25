28f421b4f983aef2ff45efcb29bd0cf39d06836d
The Nam Anh Mai <tnmai@students.cs.ubc.ca>
2024-10-24 23:34:18 -0700

    Implemented JsonReader and fixed tests in JsonReaderTest

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -2,6 +2,7 @@ package persistence;
 
 import model.Account;
 import model.Portfolio;
+import model.Stock;
 import model.StockPosition;
 
 import java.io.IOException;
@@ -14,31 +15,70 @@ import org.json.*;
 
 // Reference: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo/blob/master/src/main/persistence/JsonReader.java
 // Represents a reader that reads account from JSON data stored in file
+
 public class JsonReader {
     private String source;
 
+    // REQUIRES: source is not empty
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
+        this.source = source;
     }
 
     // EFFECTS: reads account from file and returns it;
     // throws IOException if an error occurs reading data from file
     public Account read() throws IOException {
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseAccount(jsonObject);
     }
 
+    // REQUIRES: source is not empty
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
+    // REQUIRES: jsonObject is not empty
     // EFFECTS: parses account from JSON object and returns it
     private Account parseAccount(JSONObject jsonObject) {
+        String name = jsonObject.getString("name");
+        double cashBalance = jsonObject.getDouble("balance");
+        Account account = new Account(name, cashBalance);
+        parsePortfolio(account, jsonObject);
+        return account;
     }
 
+    // REQUIRES: jsonObject is not empty
     // EFFECTS: parses portfolio from JSON object and returns it
-    private Portfolio parsePortfolio(JSONObject jsonObject) {
+    private Portfolio parsePortfolio(Account account, JSONObject jsonObject) {
+        JSONObject portfolioJson = jsonObject.getJSONObject("portfolio");
+        JSONObject positionsJson = portfolioJson.getJSONObject("positions");
+        Portfolio portfolio = account.getPortfolio();
+        // Iterate over each key-value pair in portfolioJson
+        // key is stock symbol in stock position
+        for (String symbol : positionsJson.keySet()) {
+            JSONObject positionJson = positionsJson.getJSONObject(symbol);
+            StockPosition stockPosition = parseStockPosition(positionJson);
+            portfolio.getAllStockPositions().put(symbol, stockPosition);
+        }
+        return portfolio;
     }
 
+    // REQUIRES: jsonObject is not empty
     // EFFECTS: parses stock position from JSON object and returns it
     private StockPosition parseStockPosition(JSONObject jsonObject) {
+        System.out.println("position: " + jsonObject.toString());
+        String symbol = jsonObject.getString("symbol");
+        int quantity = jsonObject.getInt("quantity");
+        double averagePrice = jsonObject.getDouble("averagePrice");
+        Stock stock = new Stock(symbol, averagePrice);
+        return new StockPosition(stock, quantity);
     }
 }
--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,41 +1,24 @@
 package persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.io.File;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.List;
 
 import org.json.JSONObject;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.Account;
-import model.Portfolio;
-import model.Stock;
-import model.StockPosition;
-import ui.StockRepository;
 
 // Reference: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo/blob/master/src/test/persistence/JsonReaderTest.java
 
 public class JsonReaderTest extends JsonTest {
-    private Account account;
-
-    @BeforeEach
-    void setUp() {
-        account = new Account("Henry", 10000);
-    }
 
     @Test
     void testReaderNonExistentFile() {
         JsonReader reader = new JsonReader("./data/noSuchFile.json");
         try {
-            account = reader.read();
+            reader.read();
             fail("IOException expected");
         } catch (IOException e) {
             // pass
@@ -44,13 +27,13 @@ public class JsonReaderTest extends JsonTest {
 
     @Test
     void testReaderAccountWithEmptyPortfolio() {
-        String testFilePath = "./data/testReaderAccountWithEmptyPortfolio";
+        String testFilePath = "./data/testReaderAccountWithEmptyPortfolio.json";
         JsonReader reader = new JsonReader(testFilePath);
         try {
             JSONObject json = getJsonObjectFromFile(testFilePath);
             Account account = reader.read();
             checkAccountDetails(json, account);
-            checkPortfolioDetails(json, account);
+            assertEquals(0, account.getPortfolio().getTotalStockPositions());
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
